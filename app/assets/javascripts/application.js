bec47a8fbe18a2603474d61f739ded01e92bb451
Owen Kelly <owen.james.kelly@gmail.com>
2014-09-07 12:20:37 -0700

    added marionette to the applicaiton.[finishes #78336306]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require underscore
 //= require backbone
+//= require backbone.marionette
 //= require aminoize
 //= require_tree ../templates
 //= require_tree ./models
