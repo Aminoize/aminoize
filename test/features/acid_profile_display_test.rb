ebd00c3619fdf804fe59f5143f2c0de5ae32ea15
Thomas Osborn <trosborn@gmail.com>
2014-12-19 20:30:05 +1200

    finishes charts for now, time to make rails api push correct data, then onto jqueryifying the charts

--- test/features/acid_profile_display_test.rb
+++ test/features/acid_profile_display_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 feature "Acid profile data is displayed on front page" do
   scenario "chart displays the amino acids for soy" do
     visit root_path
-    page.must_have_content "0.53"
-    page.must_have_content "0.324"
+    page.must_have_content "Tryptophan"
   end
 end
