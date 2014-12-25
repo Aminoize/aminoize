29e2cf2a06194be25c3d8e0775837033065a7070
Thomas Osborn <trosborn@gmail.com>
2014-12-25 14:28:24 +1200

    started experimenting with different options to save index data for appending to future searches

--- test/features/acid_profile_display_test.rb
+++ test/features/acid_profile_display_test.rb
@@ -5,10 +5,18 @@ feature "Acid profile data is displayed on front page" do
     load "#{Rails.root}/db/seeds.rb"
     visit root_path
     brown_rice = foods(:brown_rice)
+
     food_search(brown_rice)
     check_acids(brown_rice)
   end
   scenario "I can consecutively search for two different foods, and see both of them displayed" do
     visit root_path
+    brown_rice = foods(:brown_rice)
+    food_search(brown_rice)
+    whole_soybean = foods(:whole_soybean)
+    food_search(whole_soybean)
+
+    check_acids(brown_rice)
+    check_acids(whole_soybean)
   end
 end
