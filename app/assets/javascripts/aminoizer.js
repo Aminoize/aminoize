b81121f4debdf336603e09ee4d7d8a32dd3c9d19
Thomas Osborn <trosborn@gmail.com>
2015-02-06 16:21:26 -0800

    Search form now returns JSON to client-side Javascript

---
+++ app/assets/javascripts/aminoizer.js
@@ -0,0 +1,32 @@
+//handles form submission via AJAX
+jQuery(function() {
+  $("form").submit(function(e) {
+    var getData = $(this).serialize();
+    var formURL = $(this).attr("action");
+    $.ajax({
+      url: formURL,
+      type: "GET",
+      data: getData,
+      success: function(data, textStatus, jqXHR){
+        aminoize(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown){}
+    });
+    e.preventDefault();
+  });
+});
+
+$(document).submit();
+
+//initializer
+function aminoize(data){
+  if (data.length === 1) {
+    console.log(data)
+  } else if (data.length > 1) {
+    console.log("Too many things!!!!");
+  } else {
+    console.log("Too few things!!!");
+  };
+};
+
+
